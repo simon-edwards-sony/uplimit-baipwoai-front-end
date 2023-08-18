79c9e00a958a6f31e1ef9fe381211429d74ab9d0
simon-edwards-sony <130364787+simon-edwards-sony@users.noreply.github.com>
2023-08-18 17:55:06 +0100

    Changes to the fields

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -16,13 +16,12 @@ def main():
     selected_podcast = st.sidebar.selectbox("Select Podcast", options=available_podcast_info.keys())
 
     if selected_podcast:
-
         podcast_info = available_podcast_info[selected_podcast]
 
-        # Right section - Newsletter content
-        st.header("Newsletter Content")
+        # Right section - Display the podcast title
+        st.header(podcast_info['podcast_details']['podcast_title'])
 
-        # Display the podcast title
+        # Display the episode title
         st.subheader("Episode Title")
         st.write(podcast_info['podcast_details']['episode_title'])
 
@@ -31,7 +30,7 @@ def main():
 
         with col1:
             # Display the podcast episode summary
-            st.subheader("Podcast Episode Summary")
+            st.subheader("Episode Summary")
             st.write(podcast_info['podcast_summary'])
 
         with col2:
@@ -40,13 +39,14 @@ def main():
         # Display the podcast guest and their details in a side-by-side layout
         col3, col4 = st.columns([3, 7])
 
+        st.subheader("Guest")
         with col3:
-            st.subheader("Podcast Guest")
+            st.image(podcast_info['podcast_guest'][6], caption=podcast_info['podcast_guest'][0], width=300, use_column_width=True)
             st.write(podcast_info['podcast_guest'][0])
 
         with col4:
-            st.subheader("Podcast Guest Details")
             st.write(podcast_info["podcast_guest"][4])
+            st.write(podcast_info["podcast_guest"][5])
 
         # Display the five key moments
         st.subheader("Key Moments")
@@ -63,45 +63,7 @@ def main():
     st.sidebar.markdown("**Note**: Podcast processing can take upto 5 mins, please be patient.")
 
     if process_button:
-
-        # Call the function to process the URLs and retrieve podcast guest information
-        podcast_info = process_podcast_info(url)
-
-        # Right section - Newsletter content
-        st.header("Newsletter Content")
-
-        # Display the podcast title
-        st.subheader("Episode Title")
-        st.write(podcast_info['podcast_details']['episode_title'])
-
-        # Display the podcast summary and the cover image in a side-by-side layout
-        col1, col2 = st.columns([7, 3])
-
-        with col1:
-            # Display the podcast episode summary
-            st.subheader("Podcast Episode Summary")
-            st.write(podcast_info['podcast_summary'])
-
-        with col2:
-            st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
-
-        # Display the podcast guest and their details in a side-by-side layout
-        col3, col4 = st.columns([3, 7])
-
-        with col3:
-            st.subheader("Podcast Guest")
-            st.write(podcast_info['podcast_guest'][0])
-
-        with col4:
-            st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"][4])
-
-        # Display the five key moments
-        st.subheader("Key Moments")
-        key_moments = podcast_info['podcast_highlights']
-        for moment in key_moments.split('\n'):
-            st.markdown(
-                f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
+        pass
 
 def create_dict_from_json_files(folder_path):
     json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
