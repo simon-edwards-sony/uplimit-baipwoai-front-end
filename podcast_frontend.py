505c8779cdcf96d8244ec2d9ade8fc46ca6f08af
simon-edwards-sony <130364787+simon-edwards-sony@users.noreply.github.com>
2023-08-19 12:27:09 +0100

    Add files via upload

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -3,97 +3,85 @@ import modal
 import json
 import os
 
-def main():
-    st.title("Podcastatron")
+def load_sidebar():
+  st.sidebar.image('./logo.png', width=200)
+  st.sidebar.title("Podcastatron")
+  st.sidebar.subheader("Available Podcasts Feeds")
+  available_podcasts = get_podcasts_from_files('.')
+  selected_podcast = st.sidebar.selectbox("Select Podcast", options=sorted(available_podcasts.keys()))
+  if selected_podcast:
+    load_podcast_info(available_podcasts[selected_podcast])
+  
+  #st.sidebar.subheader("Add New Podcast url")
+  with st.sidebar.expander("Add New Podcast url", expanded=False):
+    url = st.text_input("RSS Feed:")
+    process_button = st.button(":heavy_check_mark: Add RSS Feed")
+    if process_button:
+         with st.spinner("Processing RSS url. This may take up to 5 minutes..."):
+          if process_podcast_info(url):
+            st.success("Successfully processed RSS url")
+          else:
+            st.error("Error processing RSS url")
 
-    available_podcast_info = create_dict_from_json_files('.')
+def load_podcast_info(podcast_info):
+  # Load Episide Title / Image / Summary
+  st.subheader(podcast_info['podcast_details']['episode_title'])
+  headercol_l, headercol_r = st.columns([3, 7])
+  with headercol_l:
+    st.image(podcast_info['podcast_details']['episode_image'], width=300, use_column_width=True)
+    
+  with headercol_r:
+    st.write(podcast_info['podcast_summary'])
+  
+  st.divider()
 
-    # Left section - Input fields
-    st.sidebar.header("Podcast RSS Feeds")
+  # Load guest Info
+  st.subheader("_Guest Info_")
+  guest_l, guest_r = st.columns([3, 7])
+  with guest_l:
+    if podcast_info['podcast_guest'][6] != '':
+      st.image(podcast_info['podcast_guest'][6], caption=podcast_info['podcast_guest'][0], width=300, use_column_width=True)
+    else:
+      st.write(podcast_info['podcast_guest'][0])
+  with guest_r:
+    st.write(podcast_info["podcast_guest"][4])
+    st.write(podcast_info["podcast_guest"][5])
+  
+  st.divider()
 
-    # Dropdown box
-    st.sidebar.subheader("Available Podcasts Feeds")
-    selected_podcast = st.sidebar.selectbox("Select Podcast", options=available_podcast_info.keys())
+  # Load highlights
+  st.subheader("_Episode Highlights_")
+  st.write(podcast_info['podcast_highlights'])
 
-    if selected_podcast:
-        podcast_info = available_podcast_info[selected_podcast]
+def get_podcasts_from_files(folder_path):
+  # Load podcast json files to dictionary
+  json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
+  data_dict = {}
 
-        # Right section - Display the podcast title
-        st.header(podcast_info['podcast_details']['podcast_title'])
+  for file_name in json_files:
+    with open(os.path.join(folder_path, file_name), 'r') as file:
+      podcast_info = json.load(file)
+      podcast_name = podcast_info['podcast_details']['podcast_title']
+      data_dict[podcast_name] = podcast_info
 
-        # Display the episode title
-        st.subheader("Episode Title")
-        st.write(podcast_info['podcast_details']['episode_title'])
-
-        # Display the podcast summary and the cover image in a side-by-side layout
-        col1, col2 = st.columns([7, 3])
-
-        with col1:
-            # Display the podcast episode summary
-            st.subheader("Episode Summary")
-            st.write(podcast_info['podcast_summary'])
-
-        with col2:
-            st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
-
-        # Display the podcast guest and their details in a side-by-side layout
-        st.subheader("Guest")
-        col3, col4 = st.columns([3, 7])
-
-        with col3:
-            if podcast_info['podcast_guest'][6] != '':
-              st.image(podcast_info['podcast_guest'][6], caption=podcast_info['podcast_guest'][0], width=300, use_column_width=True)
-            else:
-              st.write(podcast_info['podcast_guest'][0])
-
-        with col4:
-            st.write(podcast_info["podcast_guest"][4])
-            st.write(podcast_info["podcast_guest"][5])
-
-        # Display the five key moments
-        st.subheader("Key Moments")
-        key_moments = podcast_info['podcast_highlights']
-        for moment in key_moments.split('\n'):
-            st.markdown(
-                f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
-
-    # User Input box
-    st.sidebar.subheader("Add and Process New Podcast Feed")
-    with st.sidebar.expander("Click to expand and add a new podcast RSS"):
-      url = st.sidebar.text_input("Link to RSS Feed")
-      process_button = st.sidebar.button("Process Podcast Feed")
-      st.sidebar.markdown("**Note**: Podcast processing can take upto 5 mins, please be patient.")
-
-      if process_button:
-        with st.spinner(f"Fetching {url}. This can take up to 5 minutes..."):
-          process_podcast_info(url)
-
-def create_dict_from_json_files(folder_path):
-    json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
-    data_dict = {}
-
-    for file_name in json_files:
-        file_path = os.path.join(folder_path, file_name)
-        with open(file_path, 'r') as file:
-            podcast_info = json.load(file)
-            podcast_name = podcast_info['podcast_details']['podcast_title']
-            # Process the file data as needed
-            data_dict[podcast_name] = podcast_info
-
-    return data_dict
+  return data_dict
 
 def process_podcast_info(url):
+  # Process rss feed via modal
+  try:
     f = modal.Function.lookup("corise-podcast-project", "process_podcast")
     output = f.call(url, '/content/podcast/')
 
     if output:
-      num_podcasts = len(create_dict_from_json_files('.'))
-      filename = "./podcast-%i.json" % (num_podcasts+1)
+      filename = "./podcast-%i.json" % (len(get_podcasts_from_files('.'))+1)
       with open(filename, "w") as outfile:
         json.dump(output, outfile)
       return True
     else:
       return False
+  except:
+    return False
 
+# Main entry point
 if __name__ == '__main__':
-    main()
+    load_sidebar()
